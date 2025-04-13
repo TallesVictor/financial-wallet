55144e245f00bfcfaeb26256880358adcafe904a
Talles Victor <42592766+TallesVictor@users.noreply.github.com>
2025-04-12 21:18:33 -0300

    [fix] adjust error handling

--- app/Exceptions/CustomExceptionHandler.php
+++ app/Exceptions/CustomExceptionHandler.php
@@ -6,24 +6,29 @@ namespace App\Exceptions;
 use Throwable;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Contracts\Debug\ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class CustomExceptionHandler implements ExceptionHandler
 {
     public function report(Throwable $e): void
-    {
-        if (config('app.debug')) {
-            dd($e);
+    { 
+        if (env('APP_DEBUG_DD')) {
+             dd($e);
         }
-
-        // report($e);
+        
     }
 
     public function render($request, Throwable $e): JsonResponse
     {
+        $code = $e instanceof HttpExceptionInterface
+            ? $e->getStatusCode()
+            : ($e->getCode() ?: 500);
+            
         return response()->json([
-            'message' => $e->getMessage()?: 'Something went wrong',
-        ], $e->getStatusCode() ?: 500);
+            'message' => $e->getMessage() ?: 'Something went wrong',
+        ], $code);
     }
+    
 
     public function renderForConsole($output, Throwable $e): void
     {
